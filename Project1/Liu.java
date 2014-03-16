b262774c60147f0972a39a75a7c8cac0ec2b1185
Kevin Liu <minghui.liu@trincoll.edu>
2014-03-16 12:26:04 -0400

    test with 9 lines of text

--- Project1/Liu.java
+++ Project1/Liu.java
@@ -6,8 +6,8 @@ public class Liu {
 	
 	public static void main(String args[]) {
 		ArraySequence<String> text = new ArraySequence<String>();
-		for (int i=0; i<9; i++) {
-			text.addLast("A line");				
+		for (int i=0; i<10; i++) {
+			text.addFirst("A line");				
 		}
 		MyTextEditor editor = new MyTextEditor(text);
 		editor.printText();	
